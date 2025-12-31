811d5f73d514d797e1c14fb5c29f30c9f3f4b566
Ogaga Hopeson Tega <hopesonogaga@gmail.com>
2025-12-31 08:26:50 +0100

    new vid bug

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ function HomePage() {
         <video
           className="absolute top-0 left-0 w-full h-full object-cover z-0"
           src="/video/westernshot.mp4"
+          type="video/mp4"
           autoPlay
           loop
           muted
